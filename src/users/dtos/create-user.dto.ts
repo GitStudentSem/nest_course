4b3b679660f1012694c4d66668e4c57d50bffe9f
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2025-01-31 19:48:08 +0300

    lesson 18: Validating with class validator

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,7 +1,25 @@
+import {
+	IsEmail,
+	IsNotEmpty,
+	IsOptional,
+	IsString,
+	MinLength,
+} from "class-validator";
+
 export class CreateUserDto {
 	id: number;
+
+	@IsString({ message: "'name' should be a string value." })
+	@IsNotEmpty({ message: "'name' shouldn`t be an empty value." })
+	@MinLength(2, { message: "'name' should have a minimum of 2 characters." })
 	name: string;
-	gender: string;
+
+	@IsString({ message: "'gender' should be a string value." })
+	@IsOptional()
+	gender?: string;
+
+	@IsEmail()
 	email: string;
+
 	isMarried: boolean;
 }
