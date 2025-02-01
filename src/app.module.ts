b42bd5ffdbef841dbcdaff2ea46446f41755a0a5
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2025-02-01 15:32:12 +0300

    lesson 32: asunchronous connection to pestgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,30 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TweetModule } from "./tweet/tweet.module";
 import { UsersModule } from "./users/users.module";
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from "./auth/auth.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
 	/** Внутренние использованные модули */
-	imports: [UsersModule, TweetModule, AuthModule],
+	imports: [
+		UsersModule,
+		TweetModule,
+		AuthModule,
+		TypeOrmModule.forRootAsync({
+			inject: [],
+			imports: [],
+			useFactory: () => ({
+				type: "postgres",
+				entities: [],
+				synchronize: true,
+				host: "localhost",
+				port: 5432,
+				username: "postgres",
+				password: "120399",
+				database: "nestjs",
+			}),
+		}),
+	],
 	/** Отвечает за прослушку запросов */
 	controllers: [AppController],
 	/** Содержит бизнес логику модуля */
